345188b9bd5e7783dc0348d1909a46f44babcf6d
wpiyaphon <wupiyaphon@gmail.com>
2023-01-17 17:27:48 +0700

    Hong edit

--- src/pages/EP/AllStudentsPage.js
+++ src/pages/EP/AllStudentsPage.js
@@ -17,7 +17,7 @@ export default function AllStudentsPage() {
 
             <Container maxWidth={themeStretch ? false : 'xl'}>
                 <Typography variant="h3" component="h1" paragraph>
-                    Hong Edit All Students
+                    Hong 2 Edit All Students
                 </Typography>
 
             </Container>
