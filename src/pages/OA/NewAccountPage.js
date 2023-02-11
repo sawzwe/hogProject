30f05470c118c70bebf5f3ece4f89739d387d9f4
wpiyaphon <wupiyaphon@gmail.com>
2023-02-11 11:22:39 +0700

    New Account page section

--- src/pages/OA/NewAccountPage.js
+++ src/pages/OA/NewAccountPage.js
@@ -1,8 +1,12 @@
 import { Helmet } from 'react-helmet-async';
 // @mui
 import { Container, Typography } from '@mui/material';
+import { PATH_DASHBOARD } from '../../routes/paths';
 // components
+import CustomBreadcrumbs from '../../components/custom-breadcrumbs';
 import { useSettingsContext } from '../../components/settings';
+// sections
+import StaffNewEditForm from '../../sections/dashboard/oa-new-account/StaffNewEditForm';
 
 // ----------------------------------------------------------------------
 
@@ -12,31 +16,19 @@ export default function NewAccountPage() {
     return (
         <>
             <Helmet>
-                <title> OA | New account</title>
+                <title> OA | New Account </title>
             </Helmet>
 
             <Container maxWidth={themeStretch ? false : 'xl'}>
-                <Typography variant="h3" component="h1" paragraph>
-                    New account
-                </Typography>
+                <CustomBreadcrumbs
+                    heading="New Account"
+                    links={[
+                        { name: 'Account Management', href: PATH_DASHBOARD.newAccount },
+                        { name: 'New Account' }
+                    ]}
+                />
 
-                <Typography gutterBottom>
-                    Curabitur turpis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc,
-                    vitae euismod ligula urna in dolor. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit
-                    id, lorem. Phasellus blandit leo ut odio. Vestibulum ante ipsum primis in faucibus orci
-                    luctus et ultrices posuere cubilia Curae; Fusce id purus. Aliquam lorem ante, dapibus in,
-                    viverra quis, feugiat a, tellus. In consectetuer turpis ut velit. Aenean posuere, tortor
-                    sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus.
-                    Vestibulum suscipit nulla quis orci. Nam commodo suscipit quam. Sed a libero.
-                </Typography>
-
-                <Typography>
-                    Praesent ac sem eget est egestas volutpat. Phasellus viverra nulla ut metus varius
-                    laoreet. Curabitur ullamcorper ultricies nisi. Ut non enim eleifend felis pretium feugiat.
-                    Donec mi odio, faucibus at, scelerisque quis, convallis in, nisi. Fusce vel dui. Quisque
-                    libero metus, condimentum nec, tempor a, commodo mollis, magna. In enim justo, rhoncus ut,
-                    imperdiet a, venenatis vitae, justo. Cras dapibus.
-                </Typography>
+                <StaffNewEditForm />
             </Container>
         </>
     );
