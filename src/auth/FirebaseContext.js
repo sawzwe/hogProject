48f006642aebdc3773c478a582b4ea082237fb89
wpiyaphon <wupiyaphon@gmail.com>
2023-02-28 16:32:49 +0700

    Completed edit student

--- src/auth/FirebaseContext.js
+++ src/auth/FirebaseContext.js
@@ -12,9 +12,10 @@ import {
     browserLocalPersistence
 } from 'firebase/auth';
 
-import { getFirestore, collection, doc, getDoc, setDoc, clearIndexedDbPersistence } from 'firebase/firestore';
+import { getFirestore, collection, doc, getDoc, setDoc, clearIndexedDbPersistence, updateDoc } from 'firebase/firestore';
 import { getStorage, ref, uploadBytes, deleteObject, listAll, getBlob } from "firebase/storage"
 import axios from 'axios';
+import _ from 'lodash';
 // utils
 import { fDate } from '../utils/formatTime';
 // config
@@ -260,100 +261,87 @@ export function AuthProvider({ children }) {
     // UPDATE STUDENT
     const updateStudent = async (currentStudent, data) => {
 
-        // Display name to Firestore
-        // const displayNameRef = DB.collection('users').doc(currentStudent.firebaseId);
-        // console.log(currentStudent.firebaseId)
-        // console.log(DB.collection('users'));
-        // await displayNameRef.update({ displayName: `${data.studentFirstName} ${data.studentLastName}` })
-        //     .catch((error) => console.error(error))
-
-        // console.log('studentImageURL', data.studentImageURL)
-
-
-        // Image to Firebase Storage (I need to enable CORS to use getBlob)
-        // const lastImageRef = ref(storage, `users/students/${currentStudent.firebaseId}/Avatar/${currentStudent.profilePicture}`);
-        // const newImageRef = ref(storage, `users/students/${currentStudent.firebaseId}/Avatar/${data.studentImageURL.name}`);
-        // const newImage = data.studentImageURL
-        // const metadata = {
-        //     contentType: newImage.type
-        // };
-
-        // console.log(getBlob(newImage.preview))
-
-        // console.log(new File([newImage.preview.blob(), newImage.name, metadata]))
-        // if (newImage instanceof File) {
-        //     console.log("File type!")
-        //     await deleteObject(lastImageRef)
-        //     await uploadBytes(newImageRef, newImage)
-        //         .then(() => deleteObject(lastImageRef))
-        //         .catch((error) => console.error(error))
-        // } else {
-        //     console.log("Not file type!")
-        //     await deleteObject(lastImageRef)
-        //     const data = new File([newImage.preview.blob(), newImage.name, metadata]);
-        //     await uploadBytes(newImageRef, data)
-        //         .catch((error) => console.error(error))
-        // }
+        // Change display name to Firestore if name has been changed
+        if (data.studentFirstName !== currentStudent.firstName || data.studentLastName !== currentStudent.lastName) {
+            const studentRef = doc(DB, "users", currentStudent.firebaseId);
+            await updateDoc(studentRef, {
+                "displayName": `${data.studentFirstName} ${data.studentLastName}`
+            })
+        }
 
 
-        // Files to Firebase Storage
-        // const filesRef = ref(storage, `users/students/${currentStudent.firebaseId}/Files`);
-        // await listAll(filesRef)
-        //     .then((res) => {
-        //         res.items.forEach((itemRef) => {
-        //             deleteObject(itemRef)
-        //         });
-        //     })
-        //     .catch((error) => console.error(error))
+        // Delete old image and add new if user editted profile picture
+        if (data.studentImageURL instanceof File) {
+            const lastImageRef = ref(storage, `users/students/${currentStudent.firebaseId}/Avatar/${currentStudent.profilePicture}`);
+            const newImageRef = ref(storage, `users/students/${currentStudent.firebaseId}/Avatar/${data.studentImageURL.name}`);
+            deleteObject(lastImageRef)
+            await uploadBytes(newImageRef, data.studentImageURL)
+                .catch((error) => console.error(error))
+        }
 
-        // await data.studentAdditionalFiles.map((file) => {
-        //     const fileRef = ref(storage, `users/students/${currentStudent.firebaseId}/Files/${file.name}`)
-        //     const metadata = {
-        //         contentType: file.type,
-        //     };
-        //     return uploadBytes(fileRef, file, metadata)
-        //         .catch((error) => console.error(error));
-        // })
+        // Delete additional files on changes
+        const filesRef = ref(storage, `users/students/${currentStudent.firebaseId}/Files`);
+        const additionalFiles = data.studentAdditionalFiles.filter((file) => file.ref !== undefined);
+        const originalAdditionalFiles = additionalFiles.map((file) => file.ref);
+        await listAll(filesRef)
+            .then((res) => {
+                res.items.forEach((itemRef) => {
+                    if (originalAdditionalFiles.some((ref) => ref._location.path_ === itemRef._location.path_)) {
+                        return null;
+                    }
+                    return deleteObject(itemRef)
+                });
+            })
+            .catch((error) => console.error(error))
+        // Add new additional files on changes
+        await data.studentAdditionalFiles.map((file) => {
+            const fileRef = ref(storage, `users/students/${currentStudent.firebaseId}/Files/${file.name}`)
+            if (file instanceof File) {
+                return uploadBytes(fileRef, file)
+                    .catch((error) => console.error(error));
+            }
+            return null;
+        })
 
-        // // Axios to Azure
-        // const nameAdditionalFiles = data.studentAdditionalFiles.map((file) => ({ file: file.name }))
-        // await axios.put(`${HOG_API}/api/Student/Put`, {
-        //     id: currentStudent.id,
-        //     firebaseId: currentStudent.firebaseId,
-        //     title: data.studentTitle,
-        //     fName: data.studentFirstName,
-        //     lName: data.studentLastName,
-        //     nickname: data.studentNickname,
-        //     profilePicture: data.studentImageURL.name,
-        //     additionalFiles: nameAdditionalFiles || [],
-        //     dob: fDate(data.studentDateOfBirth, 'dd MMMM yyyy'),
-        //     phone: data.studentPhoneNo,
-        //     line: data.studentLineId,
-        //     email: data.studentEmail,
-        //     school: data.schoolName,
-        //     countryOfSchool: data.schoolCountry,
-        //     levelOfStudy: data.levelOfStudy,
-        //     program: data.studyProgram,
-        //     targetUni: data.targetUniversity,
-        //     targetScore: data.targetScore,
-        //     hogInfo: data.studentSource,
-        //     healthInfo: data.studentHealthInfo,
-        //     parent: {
-        //         fName: data.parentFirstName,
-        //         lName: data.parentLastName,
-        //         relationship: data.parentRelationships,
-        //         phone: data.parentPhoneNo,
-        //         email: data.parentEmail,
-        //         line: data.parentLineId,
-        //     },
-        //     address: {
-        //         address: data.address,
-        //         subdistrict: data.subDistrict,
-        //         district: data.district,
-        //         province: data.province,
-        //         zipcode: data.zipCode,
-        //     }
-        // }).catch((error) => console.error(error))
+        // Axios to Azure
+        const nameAdditionalFiles = data.studentAdditionalFiles.map((file) => ({ file: file.name }))
+        await axios.put(`${HOG_API}/api/Student/Put`, {
+            id: currentStudent.id,
+            firebaseId: currentStudent.firebaseId,
+            title: data.studentTitle,
+            fName: data.studentFirstName,
+            lName: data.studentLastName,
+            nickname: data.studentNickname,
+            profilePicture: data.studentImageURL.name,
+            additionalFiles: nameAdditionalFiles || [],
+            dob: fDate(data.studentDateOfBirth, 'dd MMMM yyyy'),
+            phone: data.studentPhoneNo,
+            line: data.studentLineId,
+            email: data.studentEmail,
+            school: data.schoolName,
+            countryOfSchool: data.schoolCountry,
+            levelOfStudy: data.levelOfStudy,
+            program: data.studyProgram,
+            targetUni: data.targetUniversity,
+            targetScore: data.targetScore,
+            hogInfo: data.studentSource,
+            healthInfo: data.studentHealthInfo,
+            parent: {
+                fName: data.parentFirstName,
+                lName: data.parentLastName,
+                relationship: data.parentRelationships,
+                phone: data.parentPhoneNo,
+                email: data.parentEmail,
+                line: data.parentLineId,
+            },
+            address: {
+                address: data.address,
+                subdistrict: data.subDistrict,
+                district: data.district,
+                province: data.province,
+                zipcode: data.zipCode,
+            }
+        }).catch((error) => console.error(error))
     }
 
 
